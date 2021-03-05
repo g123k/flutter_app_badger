df4a6953246ef9b61501ace19c07da1aeb81f1fc
fzyzcjy <turtlegood@163.com>
2021-03-05 21:51:34 +0800

    migrate to null safety

--- lib/flutter_app_badger.dart
+++ lib/flutter_app_badger.dart
@@ -14,8 +14,8 @@ class FlutterAppBadger {
     _channel.invokeMethod('removeBadge');
   }
 
-  static Future<bool> isAppBadgeSupported() async {
-    bool appBadgeSupported = await _channel.invokeMethod('isAppBadgeSupported');
+  static Future<bool?> isAppBadgeSupported() async {
+    bool? appBadgeSupported = await _channel.invokeMethod('isAppBadgeSupported');
     return appBadgeSupported;
   }
 }
