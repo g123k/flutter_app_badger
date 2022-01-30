de073b5032aa732f0ebe299dcf31d4b4cd00995f
Sikander <sikandernoori@gmail.com>
2022-01-31 01:54:34 +0500

    XIOmi support updated

--- android/src/main/java/fr/g123k/flutterappbadger/FlutterAppBadgerPlugin.java
+++ android/src/main/java/fr/g123k/flutterappbadger/FlutterAppBadgerPlugin.java
@@ -8,6 +8,10 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
 import io.flutter.plugin.common.MethodCall;
 import me.leolin.shortcutbadger.ShortcutBadger;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 
 /**
  * FlutterAppBadgerPlugin
@@ -18,6 +22,8 @@ public class FlutterAppBadgerPlugin implements MethodCallHandler, FlutterPlugin 
   private MethodChannel channel;
   private static final String CHANNEL_NAME = "g123k/flutter_app_badger";
 
+  private Notification.Builder builder;
+
   /**
    * Plugin registration.
    */
@@ -27,17 +33,25 @@ public class FlutterAppBadgerPlugin implements MethodCallHandler, FlutterPlugin 
     channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL_NAME);
     channel.setMethodCallHandler(this);
     applicationContext = flutterPluginBinding.getApplicationContext();
+    builder = new Notification.Builder(applicationContext)
+    .setContentTitle("")
+    .setContentText("")
+    .setSmallIcon(R.drawable.ic_launcher);
   }
 
   @Override
   public void onDetachedFromEngine(FlutterPluginBinding flutterPluginBinding) {
     channel.setMethodCallHandler(null);
     applicationContext = null;
+    builder = null;
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("updateBadgeCount")) {
+      if (Build.MANUFACTURER.equalsIgnoreCase("Xiaomi")) {
+        ShortcutBadger.applyNotification(applicationContext, builder.build(), Integer.valueOf(call.argument("count").toString()));
+      }
       ShortcutBadger.applyCount(applicationContext, Integer.valueOf(call.argument("count").toString()));
       result.success(null);
     } else if (call.method.equals("removeBadge")) {
