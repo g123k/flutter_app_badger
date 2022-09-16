57e5bee32e6834d8f9a9fed6e3c09df0aae49476
Edward Yakop <edward.yakop@gmail.com>
2022-09-16 17:18:29 +0800

    Fix messaging + background + IOS. (#32)

--- lib/flutter_app_badger.dart
+++ lib/flutter_app_badger.dart
@@ -6,12 +6,12 @@ class FlutterAppBadger {
   static const MethodChannel _channel =
       const MethodChannel('g123k/flutter_app_badger');
 
-  static void updateBadgeCount(int count) {
-    _channel.invokeMethod('updateBadgeCount', {"count": count});
+  static Future<void> updateBadgeCount(int count) {
+    return _channel.invokeMethod('updateBadgeCount', {"count": count});
   }
 
-  static void removeBadge() {
-    _channel.invokeMethod('removeBadge');
+  static Future<void> removeBadge() {
+    return _channel.invokeMethod('removeBadge');
   }
 
   static Future<bool> isAppBadgeSupported() async {
