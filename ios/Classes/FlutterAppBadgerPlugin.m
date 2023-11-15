690eaad9d2dbdd184eab25aaa5cfaf819f881151
George Zvonov <8287493+g-30@users.noreply.github.com>
2023-11-15 23:43:00 +0700

    support iOS 17 methods (starting from iOS 16)

--- ios/Classes/FlutterAppBadgerPlugin.m
+++ ios/Classes/FlutterAppBadgerPlugin.m
@@ -1,4 +1,5 @@
 #import "FlutterAppBadgerPlugin.h"
+#import <UserNotifications/UserNotifications.h>
 
 @implementation FlutterAppBadgerPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
@@ -21,20 +22,28 @@
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
     [self enableNotifications];
-    
-  if ([@"updateBadgeCount" isEqualToString:call.method]) {
-      NSDictionary *args = call.arguments;
-      NSNumber *count = [args objectForKey:@"count"];
-      [UIApplication sharedApplication].applicationIconBadgeNumber = count.integerValue;
-    result(nil);
-  } else if ([@"removeBadge" isEqualToString:call.method]) {
-      [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
-      result(nil);
-  } else if ([@"isAppBadgeSupported" isEqualToString:call.method]) {
-      result(@YES);
-  } else {
-    result(FlutterMethodNotImplemented);
-  }
+        
+    if ([@"updateBadgeCount" isEqualToString:call.method]) {
+        NSDictionary *args = call.arguments;
+        NSNumber *count = [args objectForKey:@"count"];
+        if (@available(iOS 16, *)) {
+            [[UNUserNotificationCenter currentNotificationCenter] setBadgeCount:count.integerValue withCompletionHandler:^(NSError * _Nullable error) {}];
+        } else {
+            [UIApplication sharedApplication].applicationIconBadgeNumber = count.integerValue;
+        }
+        result(nil);
+    } else if ([@"removeBadge" isEqualToString:call.method]) {
+        if (@available(iOS 16, *)) {
+            [[UNUserNotificationCenter currentNotificationCenter] setBadgeCount:0 withCompletionHandler:^(NSError * _Nullable error) {}];
+        } else {
+            [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
+        }
+        result(nil);
+    } else if ([@"isAppBadgeSupported" isEqualToString:call.method]) {
+        result(@YES);
+    } else {
+        result(FlutterMethodNotImplemented);
+    }
 }
 
 @end
